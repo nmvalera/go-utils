5da3bd5f9e73d4cd99e5eb352d8bb8cba6f25e0d
Nicolas Maurice <nicolas.maurice.valera@gmail.com>
2025-03-10 16:30:00 +0100

    ✨ Expose net.Listener.Addr on Server (#37)

--- net/http/server_test.go
+++ net/http/server_test.go
@@ -2,6 +2,8 @@ package http
 
 import (
 	"context"
+	"net"
+	"net/http"
 	"testing"
 
 	kkrtnet "github.com/kkrt-labs/go-utils/net"
@@ -15,6 +17,25 @@ func TestServer(t *testing.T) {
 	}
 	err := server.Start(context.Background())
 	require.NoError(t, err)
+
+	addr := server.Addr()
+	require.NotEmpty(t, addr)
+
+	// Test that the server accepts connections
+	conn, err := net.Dial("tcp", addr)
+	require.NoError(t, err)
+	err = conn.Close()
+	require.NoError(t, err)
+
+	// Test that the server accept HTTP requests
+	server.Server.Handler = http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+	req, err := http.NewRequest("GET", "http://"+addr, http.NoBody)
+	require.NoError(t, err)
+	resp, err := http.DefaultClient.Do(req)
+	require.NoError(t, err)
+	require.Equal(t, http.StatusOK, resp.StatusCode)
 
 	err = server.Stop(context.Background())
 	require.NoError(t, err)
